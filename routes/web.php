922e7342e09f552b354eab3540f233597a20b765
Olya Virchenko <olyavirchenko@192.168.1.16>
2024-07-09 15:12:48 +0300

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/groups', [GroupController::class, 'index']);
